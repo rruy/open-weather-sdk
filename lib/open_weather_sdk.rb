67608f0a6b37317ba3f1c23199572ae833bf0a8b
Ricardo Ruy <ricardo.rruy@hotmail.com>
2023-12-12 21:11:01 -0300

    Adjustments parameter when call method weather calss

--- lib/open_weather_sdk.rb
+++ lib/open_weather_sdk.rb
@@ -2,6 +2,6 @@ require_relative 'open_weather/weather_service'
 
 class OpenWeatherSdk
   def self.call(api, city)
-    OpenWeather::WeatherService.new(api, city).call
+    OpenWeather::WeatherService.new(api).call(city)
   end
 end
